d7b2e5e96d4e71e6ae60aa79329bffead6a35c43
Kevin Kub <kevin@Kevins-MacBook-Air.local>
2015-11-07 10:29:42 +0100

    Adding Drag and Dropables (working) and a placeholder ui element

--- js/lib/entity.js
+++ js/lib/entity.js
@@ -9,6 +9,20 @@ Entity.prototype.setSize = function(w, h) {
 	this.size.y = h;
 };
 
+Entity.prototype.inheritSize = function() {
+	var origin = new V2(0, 0);
+	var end = new V2(0, 0);
+	for (var i = 0; i < this.entities.length; i++) {
+		var entity = this.entities[i];
+		
+		origin.x = Math.min(entity.position.x, origin.x);
+		origin.y = Math.min(entity.position.y, origin.y);
+		end.x = Math.max(entity.getArea().p2.x, end.x);
+		end.y = Math.max(entity.getArea().p2.y, end.y);
+	}
+	this.size = end.sub(origin);
+}
+
 Entity.prototype.setPosition = function(x, y) {
 	this.position.x = x;
 	this.position.y = y;
@@ -21,6 +35,7 @@ Entity.prototype.update = function(delta) {
 };
 
 Entity.prototype.getArea = function() {
+	if(this.size.x == 0 && this.size.y == 0) this.inheritSize();
 	return new Rect(this.position, this.position.sum(this.size));
 };
 
--- js/scene/ritual.js
+++ js/scene/ritual.js
@@ -1,9 +1,25 @@
-function RitualScene() {
-	this.entities = [
-		new Placeholder(10, 10, 100, 40),
-		new Placeholder(10, 60, 100, 40),
-		new Placeholder(10, 110, 100, 40)
-	];
+function RitualScene() {	
+	var drag1 = new Placeholder(10, 10, 100, 100, 'blue');
+	var drag2 = new Placeholder(120, 10, 100, 100, 'red');
+	var drag3 = new Placeholder(230, 10, 100, 100, 'green');
+	var drop1 = new Placeholder(0, 0, 320, 100, 'yellow');
+
+	var dragable = new Dragable([drag1, drag2, drag3]);
+	var dropable = new Dropable([drop1], [dragable]);
+
+	dragable.setPosition(10, 10);
+
+	dropable.setPosition(10, 300);
+	
+	dropable.onDrop = function(dragable) { console.log(dragable); };	
+	
+	this.entities = [dropable, dragable];
+	
+	this.olderDraw = this.draw;
+	this.draw = function(ctx ) {
+		ctx.clearRect(0, 0, game.width, game.height);
+		this.olderDraw( ctx );
+	};
 }
 
 RitualScene.prototype = new Scene();
