1261da52748a85002493f74271a7c77c471deb67
Lennart Franke <lennart.franke.de@gmail.com>
2015-11-07 18:02:32 +0100

    removed console.log

--- js/entity/levelbutton.js
+++ js/entity/levelbutton.js
@@ -1,7 +1,6 @@
 g.add('img/ui/level-button.png');
 
 function LevelButton(pos, state, callback) {
-	console.log(pos);
 	this.entities = [
 		new SpriteButton('img/ui/level-button.png', pos, Rect.create(360* state, 0, 360 * (state + 1), 360), null, callback)
 	];
