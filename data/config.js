783e40680626e5af272f893e03a2ba7d09f51b58
Lennart Franke <lennart.franke.de@gmail.com>
2015-11-08 14:06:25 +0100

    button color config

--- data/config.js
+++ data/config.js
@@ -1,1 +1,11 @@
 var m = {w: 17, h: 10, t: 128};
+
+var buttonColors = { 
+	background: 'rgba(0,0,0,0.6)',
+	text: 'white'
+};
+
+var buttonColorsHover = { 
+	background: 'rgba(0,0,0,0.8)',
+	text: 'white'
+};
--- js/scene/main.js
+++ js/scene/main.js
@@ -8,43 +8,43 @@ function MainScene() {
 	this.entities = [
 		new TextButton(
 			"Start", 
-			new V2(game.width/2 - 1000, 800), 
-			new V2(600, 300), 
-			{ background: 'rgba(255,255,255,0.8)' }, 
+			new V2(game.width/2 - 1000, 850), 
+			new V2(600, 200), 
+			buttonColors, 
 			function() {
 				self.goFullScreen();
 				backgroundsound.play('sound/bg2.mp3');
 				game.scene = scenes.levelselection;
 			},
-			{ background: 'rgba(255,255,255,1)' },
+			buttonColorsHover,
 			'sound/button.mp3',
-			'50px sans-serif'
+			'80px sans-serif'
 		),
 		new TextButton(
 			"Spellbook", 
-			new V2(game.width/2 - 300, 800), 
-			new V2(600, 300), 
-			{ background: 'rgba(255,255,255,0.8)' }, 
+			new V2(game.width/2 - 300, 850), 
+			new V2(600, 200), 
+			buttonColors, 
 			function() {
 				self.goFullScreen();
 				game.scene = scenes.spellbook;
 			},
-			{ background: 'rgba(255,255,255,1)' },
+			buttonColorsHover,
 			'sound/button.mp3',
-			'50px sans-serif'
+			'80px sans-serif'
 		),
 		new TextButton(
 			"Credits", 
-			new V2(game.width/2 +400, 800), 
-			new V2(600, 300), 
-			{ background: 'rgba(255,255,255,0.8)' }, 
+			new V2(game.width/2 +400, 850), 
+			new V2(600, 200), 
+			buttonColors, 
 			function() {
 				self.goFullScreen();
 				game.scene = scenes.credits;
 			},
-			{ background: 'rgba(255,255,255,1)' },
+			buttonColorsHover,
 			'sound/button.mp3',
-			'50px sans-serif'
+			'80px sans-serif'
 		),
 		];	
 }
