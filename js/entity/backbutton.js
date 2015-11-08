b7ca6b15c052b2fdeb3b9ce7ce163dc37ab3d5f2
Kevin Kub <kevin@Kevins-MacBook-Air.local>
2015-11-08 15:02:28 +0100

    new button

--- js/entity/backbutton.js
+++ js/entity/backbutton.js
@@ -1,10 +1,12 @@
 g.add('img/ui/backbutton.png');
+g.add('img/ui/back-button.png');
 
 function AddBackbutton(action, entities) {
-	var back = new PlaceholderCircle(80, 80, 44, 'rgba(255, 255, 255, 0.9)');
-	entities.push(back);
+	//var back = new PlaceholderCircle(80, 80, 44, 'rgba(255, 255, 255, 0.9)');
+	//entities.push(back);
 	
-	entities.push(new ImageEntity('img/ui/backbutton.png', new V2(52,55)));
+	//entities.push(new ImageEntity('img/ui/backbutton.png', new V2(52,55)));
+	entities.push(new ImageEntity('img/ui/back-button.png', new V2(20,20)));
 	
 	var area = new Placeholder(20, 20, 128, 128);
 	area.color = "rgba(0,0,0,0)";
